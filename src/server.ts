135f02f12c415ddce765f80a5140b107005df86c
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2021-03-11 22:14:18 -0300

    Adjusts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+
 app.use(routes);
 
-app.get("/test", (req, res) => res.json({ message: "foi" }));
+app.get('/test', (req, res) => res.json({ message: 'foi' }));
 
-app.listen(3333, () => console.log("Entrou"));
+app.listen(3333, () => console.log('Entrou'));
